b0d6b9c2f25c6934a5bb29372839dcd3215935f3
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2020-08-01 16:53:25 -0400

    Init and first page.

---
+++ src/html.js
@@ -0,0 +1,75 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+function Script() {
+  const raw = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {dataLayer.push(arguments); }
+    gtag("js", new Date());
+
+    gtag("config", "UA-50700-3");
+ `
+  return React.createElement("script", { dangerouslySetInnerHTML: { __html: raw } });
+}
+
+function Style() {
+  const raw = `
+  * {
+    font-family: "Roboto Mono", monospace;
+    margin: 0;
+    padding: 0;
+  }
+  
+  body {
+    background-color: #222222;
+    color: #DDDDDD;
+    height: calc(100vh - 4em);
+    width: calc(100vw - 4em);
+    padding: 2em;
+  }
+  
+  #___gatsby, #gatsby-focus-wrapper {
+    height: 100%;
+    width: 100%;
+  }
+  `
+  return React.createElement("style", { dangerouslySetInnerHTML: { __html: raw } });
+}
+
+export default function HTML(props) {
+  console.log(props)
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-50700-3"></script>
+        <Script />
+        <title>Fumiaki Yoshimatsu</title>
+        <meta name="description" content="A software engineer in NYC" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;700&display=swap" rel="stylesheet" />
+        <Style />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+const profile = require("../images/profile.jpg")
+import "./index.css"
+
+export default function IndexPage() {
+  return (
+    <div className="index-page root">
+      <div className="container">
+        <h1>Hello, I'm Fumiaki Yoshimatsu.</h1>
+        <div className="content">
+          <div className="avatar">
+          <img src={profile} />
+          </div>
+          <ul className="links">
+            <li><a href="https://github.com/fumiakiy">GitHub</a></li>
+            <li><a href="https://twitter.com/fumiakiy">Twitter</a></li>
+            <li><a href="https://fumiakiy.tumblr.com">tumblr</a></li>
+            <li><a href="https://medium.com/@fumiakiy">Medium</a></li>
+            <li><a href="https://linkedin.com/in/fumiakiy">LinkedIn</a></li>
+            <li><a href="resume.html">Resume</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
